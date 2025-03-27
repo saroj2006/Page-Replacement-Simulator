400b58a97df7e9512e801f907f67796cc10b752e
saroj2006 <151620561+saroj2006@users.noreply.github.com>
2025-03-27 23:26:15 +0530

    main.cpp

--- Initial_Code/main.cpp
+++ Initial_Code/main.cpp
@@ -1,34 +1,55 @@
 #include <iostream>
 using namespace std;
 
-#define MAX_FRAMES 10
-#define MAX_PAGES 100
+// Function to check if a page is in memory
+bool isPresent(int frame[], int frames, int page) {
+    for (int i = 0; i < frames; i++) {
+        if (frame[i] == page)
+            return true;
+    }
+    return false;
+}
 
-class PageReplacement {
-protected:
-    int frames[MAX_FRAMES];
-    int numFrames;
-public:
-    PageReplacement(int n) {
-        numFrames = n;
-        for (int i = 0; i < numFrames; i++)
-            frames[i] = -1;
-    }
-    void displayFrames() {
-        cout << "Frames: ";
-        for (int i = 0; i < numFrames; i++)
-            cout << frames[i] << " ";
-        cout << endl;
-    }
-};
+// Initial Code for User Input and Menu
+void menu() {
+    int choice, n, frames;
+    cout << "Enter number of pages: ";
+    cin >> n;
+    int pages[n];
+    cout << "Enter the page reference sequence: ";
+    for (int i = 0; i < n; i++) cin >> pages[i];
+    cout << "Enter number of frames: ";
+    cin >> frames;
+    
+    do {
+        cout << "\nPage Replacement Algorithms:\n";
+        cout << "1. FIFO\n";
+        cout << "2. LRU\n";
+        cout << "3. Optimal\n";
+        cout << "4. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+        
+        switch (choice) {
+            case 1:
+                cout << "FIFO selected (Functionality to be added)\n";
+                break;
+            case 2:
+                cout << "LRU selected (Functionality to be added)\n";
+                break;
+            case 3:
+                cout << "Optimal selected (Functionality to be added)\n";
+                break;
+            case 4:
+                cout << "Exiting...\n";
+                break;
+            default:
+                cout << "Invalid choice, please try again!\n";
+        }
+    } while (choice != 4);
+}
 
 int main() {
-    int numFrames;
-    cout << "Enter number of frames: ";
-    cin >> numFrames;
-
-    PageReplacement simulator(numFrames);
-    simulator.displayFrames();
-
+    menu();
     return 0;
 }
